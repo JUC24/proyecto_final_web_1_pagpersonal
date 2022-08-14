030feee7f73ca6761781fb5b0797d8ef4ea63a0c
Jason Umaña Campos <jason1202u@gmail-com>
2022-08-13 20:34:48 -0600

    version final pagina web personal html y css corregido

--- projects/restaurant_menu/menu.js
+++
@@ -1,8 +0,0 @@
-let pedido = prompt ("Desea algo dulce o saldo ?: ")
-
-if (pedido.toLowerCase() == "salado"){
-  alert("Nuestra oferta para hoy es Pan de pizza, Pan con jamón y queso,")
-}
-else if (pedido.toLowerCase() == "dulce"){
-  alert ("Nuestra oferta para hoy es Flauta de guayaba, Cacho de dulce de leche, Pañuelo de crema pastelera.")
-}
